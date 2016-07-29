f0953b167882a2644a87fa8681a1d964a28cf5e1
kr1stjans <kristjansesek@gmail.com>
2016-07-29 13:31:36 +0200

    conversation config object

--- contactApi.js
+++ contactApi.js
@@ -3,9 +3,14 @@
  */
 var http = require("http");
 var querystring = require('querystring');
-var config =  {
+var config = {
     apiUrl: 'clean-sprint-app.4thoffice.com',
-    authToken: 'Bearer ab861108-58a7-7e48-569c-9eece5a5e4b2'
+    authToken: 'Bearer c9f52315-8d57-f401-5f16-d77cc9180418'
+};
+
+var conversationConfig = {
+    email: 'kristjansesek@gmail.com',
+    identity: 'A1_5b026989dc734be29cab0782aadfa5dc'
 };
 
 var nesty = require('./nest');
@@ -68,7 +73,7 @@ var sendChatMessage = (email, content, cb) => {
                 console.log(`BODY: ${chunk}`);
             });
             res.on('end', () => {
-                if(cb)cb();
+                if (cb)cb();
                 console.log('No more data in response.')
             })
         });
@@ -82,7 +87,7 @@ var sendChatMessage = (email, content, cb) => {
         ));
         req.end();
     })
-    
+
 };
 var getUserId = (email, cb) => {
     var options = {
@@ -106,27 +111,28 @@ var getUserId = (email, cb) => {
         })
     });
 
-    req.write(JSON.stringify( {"User": {
-        "$type": "User_14",
-        "AccountList": [{
-            "$type": "AccountEmail_14",
-            "Email": email
-        }]
-    }
+    req.write(JSON.stringify({
+        "User": {
+            "$type": "User_14",
+            "AccountList": [{
+                "$type": "AccountEmail_14",
+                "Email": email
+            }]
+        }
     }));
     req.end();
 };
 var fetchMessages = (user) => {
     console.log("Hey");
     var postData = querystring.stringify({
-        'feedscope':'ChatStream',
-        'feedidentity':'A1_0efa529f54eb4d669d865e1da85c7166',
-        'size':10,
-        'offset':0
+        'feedscope': 'ChatStream',
+        'feedidentity': conversationConfig.identity,
+        'size': 10,
+        'offset': 0
     });
     var options = {
         host: 'clean-sprint-app.4thoffice.com',
-        path: '/api/feed?'+postData,
+        path: '/api/feed?' + postData,
         method: 'GET',
         headers: {
             'Content-Type': 'application/vnd.4thoffice.feed-5.15+json',
@@ -139,7 +145,7 @@ var fetchMessages = (user) => {
         res.setEncoding('utf8');
         res.on('data', (chunk) => {
             //cb(JSON.parse(chunk));
-            data+=chunk;
+            data += chunk;
             console.log(`BODY: ${chunk}`);
         });
         res.on('end', () => {
@@ -151,60 +157,62 @@ var fetchMessages = (user) => {
     req.write(postData);
     req.end();
 };
-setInterval(function() {fetchMessages('denkomanceski@gmail.com')}, 1000);
+setInterval(function () {
+    fetchMessages(conversationConfig.email)
+}, 1000);
 var skip = false;
-var parseCheckFor = function(chunck){
+var parseCheckFor = function (chunck) {
     var t = 0;
-    chunck.DiscussionListPage.DiscussionList.forEach(function(item) {
+    chunck.DiscussionListPage.DiscussionList.forEach(function (item) {
         t++;
-        if(item.Post)
-        if(item.Post.Text){
-            if(t == 1 && item.Post.Text != lastMessageByMe && !skip){
-                skip = true;
-                checkAction(item.Post.Text, (content) => {
-                    if(content.length > 0)
-                        sendChatMessage('denkomanceski@gmail.com', content, () => {
-                        skip = false;
+        if (item.Post)
+            if (item.Post.Text) {
+                if (t == 1 && item.Post.Text != lastMessageByMe && !skip) {
+                    skip = true;
+                    checkAction(item.Post.Text, (content) => {
+                        if (content.length > 0)
+                            sendChatMessage(conversationConfig.email, content, () => {
+                                skip = false;
+                            });
+                        else {
+                            skip = false;
+                        }
                     });
-                    else {
-                        skip = false;
-                    }
-                });
+                }
+                console.log(item.Post.Text)
             }
-            console.log(item.Post.Text)
-        }
 
     });
 
 }
 var volume = require('./volume');
 var checkAction = (action, cb) => {
-    if(action.indexOf('coming home') > -1) {
+    if (action.indexOf('coming home') > -1) {
         nesty.setTemperature(25);
         cb('Great! The current temperature here is 15 degree, but by the time you come home I will set it to 25');
     }
-    else if(action.indexOf('volume up') > -1) {
+    else if (action.indexOf('volume up') > -1) {
         volume.setVolume(100, (success) => {
             cb("Okay, I've increased the volume for you to 100%. Enjoy your music!");
         });
     }
-    else if(action.indexOf('volume down') > -1) {
+    else if (action.indexOf('volume down') > -1) {
         volume.setVolume(60, (success) => {
             cb("Right..the volume is decreased to 60%. ");
         });
     }
-    else if(action.toLowerCase().indexOf('hello') > -1){
+    else if (action.toLowerCase().indexOf('hello') > -1) {
         cb('Hi boss, what would you like me to do for you :)')
     }
-    else if(action.toLowerCase().indexOf('how are you') > -1){
+    else if (action.toLowerCase().indexOf('how are you') > -1) {
         cb('I am feeling great, I have you')
     }
     else {
         cb('');
     }
 }
-//sendMailMessage('denkomanceski@gmail.com', 'Mofo', 'What the heck');
-//sendChatMessage('denkomanceski@gmail.com', "Testing mountains");
+//sendMailMessage(conversationConfig.email, 'Mofo', 'What the heck');
+//sendChatMessage(conversationConfig.email, "Testing mountains");
 
 
 
