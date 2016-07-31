881f78b84a206628ff57eb8b3a6792b265ee3fe7
denkomanceski <denkomanceski@gmail.com>
2016-07-31 14:46:28 +0100

    changed token

--- routes/index.js
+++ routes/index.js
@@ -13,18 +13,41 @@ router.get('/actionableResource/availability', (req, res) => {
     res.send(obj)
 });
 router.get('/actionableResource', (req, res) => {
-    console.log("request came", JSON.stringify(req.params));
-    var assistant_chat_bubble = 'Hello and welcome on stream list'
-
-    var next_step = {name: 'Show me next thing', type: 'ActionNextStep_18'}
-    var close_dialog = {name: 'Bye', type: 'ActionFinishWorkflow_18'}
-    var actions = [next_step, close_dialog]
-    var description_list = [assistant_chat_bubble]
+    // console.log("request came", JSON.stringify(req.params));
+    // var assistant_chat_bubble = 'Hello and welcome on stream list'
+    //
+    // var next_step = {name: 'Show me next thing', type: 'ActionNextStep_18'}
+    // var close_dialog = {name: 'Bye', type: 'ActionFinishWorkflow_18'}
+    // var actions = [next_step, close_dialog]
+    // var description_list = [assistant_chat_bubble]
+    // var obj = {
+    //     'DescriptionList': description_list,
+    //     'ActionList': actions,
+    //     '$type': 'ActionableResource_22',
+    //     'Id': uuid.v4()
+    // };
     var obj = {
-        'DescriptionList': description_list,
-        'ActionList': actions,
-        '$type': 'ActionableResource_22',
-        'Id': uuid.v4()
+        "$type": "ActionableResource_22",
+        "Id": "d3921a15-41d0-48bd-afa3-5f075bf05228",
+        "DescriptionList": [
+            "Hello and welcome on stream list"
+        ],
+        "ActionList": [
+            {
+                "ActionType": "Positive",
+                "Name": "Show me next thing",
+                "Id": "264d0634-4111-403d-8ca0-12090eca1c4a",
+                "$type": "ActionNextStep_18",
+                "AssistantEmail": "1cd65bd6-fd85-4a80-8e1c-3263c0793bdf@4thoffice.com"
+            },
+            {
+                "ActionType": "Positive",
+                "Name": "Bye",
+                "Id": "947c007d-26cd-44e2-96b6-9170613c2dc2",
+                "$type": "ActionFinishWorkflow_18",
+                "AssistantEmail": "1cd65bd6-fd85-4a80-8e1c-3263c0793bdf@4thoffice.com"
+            }
+        ]
     };
     res.set('Content-Type', 'application/vnd.4thoffice.actionable.resource.availability-v5.15+json')
     res.send(obj)
