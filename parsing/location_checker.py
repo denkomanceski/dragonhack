738295282106f8d441498bd1ef9a094549241cd8
kuro-kuris <cswart@outlook.com>
2016-07-31 16:28:57 +0200

    location checker from file

--- parsing/location_checker.py
+++ parsing/location_checker.py
@@ -1,9 +1,15 @@
-import mmap
+"""Location parsing."""
+import re
+
+
+def findWholeWord(w):
+    """Regex to find whole word from string."""
+    return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
 
 
 def check(blabla):
     """Check for blabla in London street file."""
-    f = open('/home/chris/Downloads/city_street_names/london.osm.streets.txt')
-    s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-    if s.find('blabla') != -1:
-        return 'true'
+    file = open(
+        '/home/chris/Downloads/city_street_names/london.osm.streets.txt')
+    if findWholeWord(blabla)(file.read()):
+        print True
