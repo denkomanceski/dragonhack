0ed4e85512b3e2f08d4baaf53a026e6fdad5e9d9
denkomanceski <denkomanceski@gmail.com>
2016-07-31 14:29:33 +0100

    changed token

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,21 @@ router.get('/actionableResource/availability', (req, res) => {
     res.send(obj)
 });
 router.get('/actionableResource', (req, res) => {
-    console.log("request came");
-    res.send({ok: true})
+    console.log("request came", JSON.stringify(req.params));
+    var assistant_chat_bubble = 'Hello and welcome on stream list'
+
+    var next_step = {name: 'Show me next thing', type: 'ActionNextStep_18'}
+    var close_dialog = {name: 'Bye', type: 'ActionFinishWorkflow_18'}
+    var actions = [next_step, close_dialog]
+    var description_list = [assistant_chat_bubble]
+    var obj = {
+        'DescriptionList': description_list,
+        'ActionList': actions,
+        '$type': 'ActionableResource_22',
+        'Id': uuid.v4()
+    };
+    res.set('Content-Type', 'application/vnd.4thoffice.actionable.resource.availability-v5.15+json')
+    res.send(obj)
 });
 router.get('/actionableResource/:actionableResourceId', (req, res) => {
     console.log("request came", JSON.stringify(req.params));
