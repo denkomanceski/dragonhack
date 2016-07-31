435aa4a431ea74fe1b30889aa8e12e30108c2106
denkomanceski <denkomanceski@gmail.com>
2016-07-31 12:51:40 +0100

    changed token

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,41 @@
 var express = require('express');
 var router = express.Router();
-
+var uuid = require('node-uuid');
 /* GET home page. */
 router.get('/actionableResource/availability', (req, res) => {
-  console.log("request came", JSON.stringify(req.params));
-  var obj = {
-    '$type': 'ActionableResourceAvailability_20',
-    'Mode': 'Action',
-    'ActionableResourceId': `test.chatstream.${req.query.contextType}`
-  };
-  res.set('Content-Type', 'application/vnd.4thoffice.actionable.resource.availability-v5.15+json');
-  res.send(obj)
+    console.log("request came", JSON.stringify(req.params));
+    var obj = {
+        '$type': 'ActionableResourceAvailability_20',
+        'Mode': 'Action',
+        'ActionableResourceId': '12345'
+    };
+    res.set('Content-Type', 'application/vnd.4thoffice.actionable.resource.availability-v5.15+json');
+    res.send(obj)
 });
 router.get('/actionableResource', (req, res) => {
-  console.log("request came");
-  res.send({ok: true})
+    console.log("request came");
+    res.send({ok: true})
 });
 router.get('/actionableResource/:actionableResourceId', (req, res) => {
-  console.log("request came", JSON.stringify(req.params));
-  res.send({ok: true})
+    console.log("request came", JSON.stringify(req.params));
+    var assistant_chat_bubble = 'Hello and welcome on stream list'
+
+    var next_step = {name: 'Show me next thing', type: 'ActionNextStep_18'}
+    var close_dialog = {name: 'Bye', type: 'ActionFinishWorkflow_18'}
+    var actions = [next_step, close_dialog]
+    var description_list = [assistant_chat_bubble]
+    var obj = {
+        'DescriptionList': description_list,
+        'ActionList': actions,
+        '$type': 'ActionableResource_22',
+        'Id': uuid.v4()
+    };
+    res.set('Content-Type', 'application/vnd.4thoffice.actionable.resource.availability-v5.15+json')
+    res.send(obj)
 });
 router.post('/action', (req, res) => {
-  console.log("request came");
-  res.send({ok: true})
+    console.log("request came");
+    res.send({ok: true})
 });
 
 module.exports = router;
