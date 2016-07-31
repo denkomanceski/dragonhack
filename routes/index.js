89acde5d72598ac1a751cba31150068204531356
denkomanceski <denkomanceski@gmail.com>
2016-07-31 12:44:33 +0100

    changed token

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/actionableResource/availability', (req, res) => {
   console.log("request came", JSON.stringify(req.params));
-  res.send({ok: true})
+  var obj = {
+    '$type': 'ActionableResourceAvailability_20',
+    'Mode': 'Action',
+    'ActionableResourceId': `test.chatstream.${req.query.contextType}`
+  };
+  res.set('Content-Type', 'application/vnd.4thoffice.actionable.resource.availability-v5.15+json');
+  res.send(obj)
 });
 router.get('/actionableResource', (req, res) => {
   console.log("request came");
