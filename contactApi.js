8dbd3492b437638cfadcbc59b875a1999d55fc11
kr1stjans <kristjansesek@gmail.com>
2016-07-31 18:19:16 +0100

    refactor & google calendar [npm install]

--- contactApi.js
+++ contactApi.js
@@ -1,21 +1,21 @@
 /**
  * Created by denkomanceski on 5/14/16.
  */
+var debug = true;
 var http = require("http");
+var _ = require('lodash');
 var querystring = require('querystring');
 var config = {
     apiUrl: 'clean-sprint-app.4thoffice.com',
-    authToken: 'Bearer a9ac4015-e8ba-8dcf-642a-e3fe58e1b57f'
+    authToken: 'Bearer 9a1daf8c-d175-8713-23bb-c366072d06c9'
 };
 
 var conversationConfig = {
     email: 'denkomanceski@gmail.com',
     userId: '8a360d87-7ed7-4bea-8846-a807903d0e73',
-    conversationIdentity: 'A1_6704727b30ea44349af7e97dcfdb34f9',
+    conversationIdentity: 'A1_20f0a67d5ce841a1b409e6e98f76602d',
     conversationWith: 'uzupan@marg.si'
 };
-
-var nesty = require('./nest');
 
 var extractionController = require('./controllers/extractionController');
 var externalAPIController = require('./controllers/externalAPIController');
@@ -26,7 +26,6 @@ var ACTION = {
     GOOGLE_CALENDAR: 2,
     UBER: 3
 };
-
 
 var sendMailMessage = (email, title, content) => {
     var options = {
@@ -43,10 +42,10 @@ var sendMailMessage = (email, title, content) => {
     var req = http.request(options, (res) => {
         res.setEncoding('utf8');
         res.on('data', (chunk) => {
-            //console.log(`BODY: ${chunk}`);
+            //logMsg(`BODY: ${chunk}`);
         });
         res.on('end', () => {
-            console.log('No more data in response.')
+            logMsg('No more data in response.')
         })
     });
 
@@ -67,17 +66,17 @@ var sendMailMessage = (email, title, content) => {
     }));
     req.end();
 };
-var lastMessageByMe = '';
+var lastProcessedMessage = '';
 
 var sendChatMessageByEmail = (email, content, cb) => {
-    lastMessageByMe = content;
+    lastProcessedMessage = content;
     getUserId(email, (res) => {
         sendChatMessageByFeedIdentity(res.Id, content, cb);
     })
 
 };
-var sendChatMessageByFeedIdentity = (feedIdentity, content, cb) => {
-    //console.log(res.Id);
+var sendChatMessageByFeedIdentity = (feedIdentity, content, callback) => {
+    //logMsg(res.Id);
     var options = {
         host: config.apiUrl,
         path: '/api/post',
@@ -92,11 +91,11 @@ var sendChatMessageByFeedIdentity = (feedIdentity, content, cb) => {
     var req = http.request(options, (res) => {
         res.setEncoding('utf8');
         res.on('data', (chunk) => {
-            console.log(`BODY: ${chunk}`);
+            logMsg(`BODY: ${chunk}`);
         });
         res.on('end', (err, data) => {
-            if (cb)cb(data);
-            console.log('No more data in response.')
+            if (callback)callback(data);
+            logMsg('No more data in response.')
         })
     });
 
@@ -126,10 +125,10 @@ var getUserId = (email, cb) => {
         res.setEncoding('utf8');
         res.on('data', (chunk) => {
             cb(JSON.parse(chunk));
-            //console.log(`BODY: ${chunk}`);
+            //logMsg(`BODY: ${chunk}`);
         });
         res.on('end', () => {
-            console.log('No more data in response.')
+            logMsg('No more data in response.')
         })
     });
 
@@ -145,7 +144,6 @@ var getUserId = (email, cb) => {
     req.end();
 };
 var fetchMessages = (user) => {
-    console.log("Hey");
     var postData = querystring.stringify({
         'feedscope': 'ChatStream',
         'feedidentity': conversationConfig.conversationIdentity,
@@ -169,11 +167,11 @@ var fetchMessages = (user) => {
         res.on('data', (chunk) => {
             //cb(JSON.parse(chunk));
             data += chunk;
-            //console.log(`BODY: ${chunk}`);
+            //logMsg(`BODY: ${chunk}`);
         });
         res.on('end', () => {
-            return parseCheckFor(JSON.parse(data));
-            console.log('No more data in response.')
+            return parseAction(JSON.parse(data));
+            logMsg('No more data in response.');
         })
     });
 
@@ -183,50 +181,37 @@ var fetchMessages = (user) => {
 setInterval(function () {
     fetchMessages(conversationConfig.conversationWith)
 }, 3000);
-var skip = false;
-var parseCheckFor = function (chunck) {
-    var t = 0;
-    chunck.DiscussionListPage.DiscussionList.forEach(function (item) {
-        t++;
-        if (item.Post)
-            if (item.Post.Text) {
-                if (t == 1 && item.Post.Text != lastMessageByMe && !skip) {
-                    skip = true;
-                    checkAction(item.Post.Text, (content) => {
-                        if (content.length > 0)
-                        // sendChatMessageByEmail('denkomanceski@gmail.com', content, () => {
-                        //     skip = false;
-                        // });
-                            sendChatMessageByFeedIdentity(conversationConfig.conversationIdentity, content, () => {
-                                skip = false;
-                            });
 
+var parseAction = function (chunk) {
+    var newParsedMessage = _.get(chunk, 'DiscussionListPage.DiscussionList[0].Post.Text', undefined);
+    if (newParsedMessage && newParsedMessage != lastProcessedMessage) {
+        // process new parsed message
+        processAction(newParsedMessage, function (messageToSend) {
+            sendChatMessageByFeedIdentity(conversationConfig.conversationIdentity, messageToSend, function () {
+                lastProcessedMessage = messageToSend;
+            });
+        });
 
-                        else {
-                            skip = false;
-                        }
-                    });
-                }
-                console.log(t + '. ' + item.Post.Text)
-            }
-
-    });
-
+        // set new parsed message as last processed message
+        lastProcessedMessage = newParsedMessage;
+    }
 };
 
 var volume = require('./volume');
 var lastActionCode, lastActionText;
 
-// external python scripts are running asynchronously, so we always wait until they finish, before we continue to
-// process new data
+// external python scripts are running asynchronously and we always wait until they finish,
+// before we continue to process new data
 var externalServiceRunning = false;
 
-var checkAction = (action, cb) => {
+var processAction = (action, cb) => {
 
+    action = action.toLowerCase();
+
+    // skip processing when external service is running
     if (externalServiceRunning) {
         return;
     }
-
 
     if (action.indexOf('volume up') > -1) {
         volume.setVolume(100, (success) => {
@@ -235,26 +220,25 @@ var checkAction = (action, cb) => {
     }
     else if (action.indexOf('volume down') > -1) {
         volume.setVolume(60, (success) => {
-            cb("Right..the volume is decreased to 60%. ");
+            cb("Right..the volume is decreased to 60%.");
         });
     }
-    else if (action.toLowerCase().indexOf('hello') > -1) {
-        cb('Hi boss, what would you like me to do for you :)')
+    else if (action.indexOf('hello') > -1) {
+        cb('Hi boss, what would you like me to do for you :)');
     }
-    else if (action.toLowerCase().indexOf('how are you') > -1) {
-        cb('I am feeling great, I have you')
+    else if (action.indexOf('how are you') > -1) {
+        cb('I am feeling great, I have you');
     }
-    else if (action.toLowerCase().indexOf('to go from') > -1) {
+    else if (action.indexOf('to go from') > -1) {
         lastActionCode = ACTION.SKYSCANNER;
         lastActionText = action;
-        cb(checkCities(action))
+        cb(checkCities(action));
     }
-    else if (action.toLowerCase().indexOf('yes') > -1) {
+    else if (action.indexOf('yes') > -1) {
         switch (lastActionCode) {
             case ACTION.SKYSCANNER:
                 var spawn = require('child_process').spawn;
                 spawn('open', [checkCities(lastActionText)]);
-                cb('');
                 break;
             case ACTION.GOOGLE_CALENDAR:
                 cb('A meeting on ' + lastActionText + ' added to calendar.');
@@ -265,20 +249,20 @@ var checkAction = (action, cb) => {
         lastActionCode = '';
         lastActionText = '';
     }
-    else if (action.toLowerCase().indexOf('meet') > -1) {
+    else if (action.indexOf('no') > -1) {
+        lastActionCode = '';
+        lastActionText = '';
+    }
+    else if (action.indexOf('meet') > -1) {
 
         externalServiceRunning = true;
 
         // try to extract the date
         extractionController.extractDateTime(action, (err, results) => {
             if (err) throw err;
-            lastActionCode = ACTION.SKYSCANNER;
-
-            var lastMessage = 'I noticed you are planning a meeting on ' + results[0]
-                + '. Would you like me to add a meeting to calendar and send invitation?';
-            lastMessageByMe = lastMessage;
-
-            cb(lastMessage);
+            lastActionCode = ACTION.GOOGLE_CALENDAR;
+            cb('I noticed you are planning a meeting on ' + results[0]
+                + '. Would you like me to add a meeting to calendar and send invitation?');
             lastActionText = results[0];
 
             externalServiceRunning = false;
@@ -304,13 +288,19 @@ var cityNamesDictinary = [
     {name: 'london', code: 'lond'},
     {name: 'ljubljana', code: 'lju'}
 ];
-sendChatMessageByEmail('kristjansesek@gmail.com', "test123");
+
+//sendChatMessageByEmail('kristjansesek@gmail.com', "test123");
 //sendChatMessageByFeedIdentity('A1_cc175089d4d34e5492588e65ae8920fd','denkomanceski@gmail.com');
 // sendChatMessageByFeedIdentity('A1_20f0a67d5ce841a1b409e6e98f76602d', "Hello test123", (data) => {
-//     console.log(JSON.stringify(data));
+//     logMsg(JSON.stringify(data));
 // });
 
 getUserId('denkomanceski@gmail.com', (res) => {
-    console.log(JSON.stringify(res));
+    logMsg(JSON.stringify(res));
 });
 
+var logMsg = function (content) {
+    if (debug) {
+        console.log(content);
+    }
+};
--- parsing/time_parser.py
+++ parsing/time_parser.py
@@ -1,7 +1,7 @@
 """Module to parse time data."""
 from timex import tag, gmt, ground
 from dateutil import parser
-import sys
+import sys, datetime
 
 def timex_tag(content, base_time=gmt()):
     """Timex tagger using the timex module.
@@ -15,12 +15,12 @@ def timex_tag(content, base_time=gmt()):
 
 def example_timex_tag():
     """Example for testing timex."""
-    return timex_tag('Belfast up and coming band Cashier No.9 are playing a free gig in the Mercantile this Saturday night at 9:30pm.')
+    return timex_tag('Belfast up and coming band Cashier are playing a free gig in the Mercantile tomorrow at 9:30pm.')
 
 
 def example2_timex_tag():
     """Removing numerals from example for dateutil."""
-    content = example_timex_tag().replace("TIMEX2", "")[40:]
+    content = example_timex_tag().replace("TIMEX2", "")
     return content
 
 
@@ -30,7 +30,6 @@ def parse_time(dated_content):
     Doesn't work if there are any additional numbers in string
     """
     timed_content = parser.parse(dated_content, fuzzy=True)
-    print timed_content
     return timed_content
 
 
@@ -39,7 +38,16 @@ def example_dateutil():
     content = example2_timex_tag()
     return parse_time(content)
 
+def should_parse_tomorrow():
+    assert str(datetime.date.today() + datetime.timedelta(days=1)) in str(parse_time('We are going to meet tomorrow at 9:30pm'));
+
+def should_parse_today():
+    assert str(datetime.date.today()) in str(parse_time('We are going to meet today at 9:30pm'));
 
 if __name__ == "__main__":
-    for v in sys.argv[1:]:
-        parse_time(v)
+    if(len(sys.argv) >= 2 and sys.argv[1]=='debug'):
+        should_parse_today()
+        should_parse_tomorrow()
+    else:
+        for v in sys.argv[1:]:
+            print(parse_time(v))
