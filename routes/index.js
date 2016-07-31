b6b0450c700f790619832d6c70999ff33332b334
denkomanceski <denkomanceski@gmail.com>
2016-07-31 12:31:53 +0100

    changed token

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/actionableResource/availability', (req, res) => {
-  console.log("request came");
+  console.log("request came", JSON.stringify(req.params.query));
   res.send({ok: true})
-
 });
 router.get('/actionableResource', (req, res) => {
   console.log("request came");
   res.send({ok: true})
-
 });
 router.post('/action', (req, res) => {
   console.log("request came");
   res.send({ok: true})
-
 });
 
 module.exports = router;
