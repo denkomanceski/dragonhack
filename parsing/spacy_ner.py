291f21354fc283494ed1f03688d026ed733bdc18
kuro-kuris <cswart@outlook.com>
2016-08-01 12:44:16 +0200

    command line named entity recognition

--- parsing/spacy_ner.py
+++ parsing/spacy_ner.py
@@ -1,6 +1,6 @@
 """Named entity recognition using spacy."""
 import spacy.en
-
+import argparse
 
 def ner(input_string):
     """Basic ner."""
@@ -9,3 +9,11 @@ def ner(input_string):
     parsed_sentence = parser(unicode_string)
     for entity in parsed_sentence.ents:
         print entity.label, entity.label_, ' '.join(t.orth_ for t in entity)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Test string')
+    parser.add_argument("text", help="Named entity recognition of text", nargs="*")
+    args = parser.parse_args()
+    ner(args.text[0])
+    print "Something happened"
+
