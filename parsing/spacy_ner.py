82db56a9462710f30b479ae646834fa326379369
kuro-kuris <cswart@outlook.com>
2016-08-01 14:30:08 +0200

    command line support to execute examples with named entity recognition

--- parsing/spacy_ner.py
+++ parsing/spacy_ner.py
@@ -10,10 +10,23 @@ def ner(input_string):
     for entity in parsed_sentence.ents:
         print entity.label, entity.label_, ' '.join(t.orth_ for t in entity)
 
+def test_examples():
+    with open("example.txt") as f:
+        content = f.readlines()
+        for sentence in content:
+            print sentence
+            ner(sentence)
+            print "---"
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Test string')
     parser.add_argument("text", help="Named entity recognition of text", nargs="*")
     args = parser.parse_args()
-    ner(args.text[0])
-    print "Something happened"
+    if args.text[0] == "example":
+        print "Testing examples:"
+        test_examples()
+    else:
+        ner(args.text[0])
+        print "Something happened"
 
