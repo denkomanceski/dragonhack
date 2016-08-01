4d9e3b06fbf672e34446153eb774e8a0f1d66b31
kuro-kuris <cswart@outlook.com>
2016-08-01 16:38:53 +0200

    regex time parsing improved

--- parsing/time_parser.py
+++ parsing/time_parser.py
@@ -3,7 +3,7 @@ from timex import tag, gmt, ground
 from dateutil import parser
 import sys, datetime
 
-def timex_tag(content, base_time=gmt()):
+def timex_parse(content, base_time=gmt()):
     """Timex tagger using the timex module.
 
     Ripped from nltk_contrib using regex.
@@ -13,36 +13,42 @@ def timex_tag(content, base_time=gmt()):
     return injected_base_text
 
 
+def dateutil_parse(content):
+    """Dateutil parser which merges the timex date with time data.
+
+    Doesn't work if there are any additional numbers in string
+    """
+    timed_content = parser.parse(content, fuzzy=True)
+    return timed_content
+
+
+def parse_time(content):
+    """Time parser combining dateutil and timex"""
+    timex_parsed = timex_parse(content)
+    dateutil_parsed = dateutil_parse(timex_parsed)
+    return dateutil_parsed
+
+
 def example_timex_tag():
     """Example for testing timex."""
     return timex_tag('Belfast up and coming band Cashier are playing a free gig in the Mercantile tomorrow at 9:30pm.')
 
 
-def example2_timex_tag():
-    """Removing numerals from example for dateutil."""
-    content = example_timex_tag().replace("TIMEX2", "")
-    return content
-
-
-def parse_time(dated_content):
-    """Dateutil parser which merges the timex date with time data.
-
-    Doesn't work if there are any additional numbers in string
-    """
-    timed_content = parser.parse(dated_content, fuzzy=True)
-    return timed_content
-
-
 def example_dateutil():
     """Example for testing dateutil."""
-    content = example2_timex_tag()
+    content = timex_tag('Belfast up and coming band Cashier are playing a free gig in the Mercantile tomorrow at 9:30pm.')
     return parse_time(content)
 
+
 def should_parse_tomorrow():
-    assert str(datetime.date.today() + datetime.timedelta(days=1)) in str(parse_time('We are going to meet tomorrow at 9:30pm'));
+    print "tomorrow"
+    print str(datetime.date.today() + datetime.timedelta(days=1))
+    print str(parse_time('We are going to meet tomorrow at 9:30pm'));
 
 def should_parse_today():
-    assert str(datetime.date.today()) in str(parse_time('We are going to meet today at 9:30pm'));
+    print "today"
+    print str(datetime.date.today())
+    print str(parse_time('We are going to meet today at 9:30pm'));
 
 if __name__ == "__main__":
     if(len(sys.argv) >= 2 and sys.argv[1]=='debug'):
