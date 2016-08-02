62315459c050dd842e2f130c3903592dbbea5c38
kr1stjans <kristjansesek@gmail.com>
2016-08-02 16:23:03 +0100

    streets parsing added

--- controllers/extractionController.js
+++ controllers/extractionController.js
@@ -1,20 +1,29 @@
 var async = require('async');
 
-
 var PythonShell = require('python-shell');
 
-module.exports = {
-    extractData: function (content, callback) {
-        async.parallel([
-            function (cb) {
-                PythonShell.run('../parsing/time_parser.py', {
-                    args: content
-                }, cb);
-            },
-            function (cb) {
-                PythonShell.run('../parsing/spacy_ner.py', {
-                    args: content
-                }, cb);
-            }], callback);
-    }
+exports.extractMeetingData = function (content, callback) {
+    async.parallel([
+        function (cb) {
+            PythonShell.run('../parsing/time_parser.py', {
+                args: content
+            }, cb);
+        }, function (cb) {
+            PythonShell.run('../parsing/location_checker.py', {
+                args: content
+            }, cb);
+        }], callback);
+};
+
+exports.extractTravelData = function (content, callback) {
+    async.parallel([
+        function (cb) {
+            PythonShell.run('../parsing/time_parser.py', {
+                args: content
+            }, cb);
+        }, function (cb) {
+            PythonShell.run('../parsing/spacy_ner.py', {
+                args: content
+            }, cb);
+        }], callback);
 };
