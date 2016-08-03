0a6da42434ada2bb590c815ab5a52bf1553c1070
denkomanceski <denkomanceski@gmail.com>
2016-08-03 14:30:49 +0100

    adding new route for the local server

--- controllers/extractionController.js
+++ controllers/extractionController.js
@@ -3,22 +3,16 @@ var path = require('path');
 var PythonShell = require('python-shell');
 
 exports.extractMeetingData = function (content, callback) {
-    // PythonShell.run('../parsing/location_checker.py', {
-    //     args: content
-    // }, callback);
+    async.parallel([
+        function (cb) {
             PythonShell.run('parsing/time_parser.py', {
                 args: content
-            }, callback);
-    // async.parallel([
-    //     function (cb) {
-    //         PythonShell.run('../parsing/time_parser.py', {
-    //             args: content
-    //         }, cb);
-    //     }, function (cb) {
-    //         PythonShell.run('../parsing/location_checker.py', {
-    //             args: content
-    //         }, cb);
-    //     }], callback);
+            }, cb);
+        }, function (cb) {
+            PythonShell.run('parsing/location_checker.py', {
+                args: content
+            }, cb);
+        }], callback);
 };
 
 exports.extractTravelData = function (content, callback) {
